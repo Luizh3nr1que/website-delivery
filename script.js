9503350e43028bf1ee6459be4b22c9c13c78d20c
luizh3nrique <luizh3nrique00@gmail.com>
2023-06-11 18:30:22 -0300

    Signed-off-by: luizh3nrique <luizh3nrique00@gmail.com>

---
+++ script.js
@@ -0,0 +1,51 @@
+
+function mostrarConteudo(index) {
+    var sobrePorConteudo = document.querySelectorAll('.sobrePorConteudo');
+    sobrePorConteudo[index].style.display = 'flex';
+}
+
+function ocultarConteudo(index) {
+    var sobrePorConteudo = document.querySelectorAll('.sobrePorConteudo');
+    sobrePorConteudo[index].style.display = 'none';
+}
+
+
+
+const ButtonMobile = document.querySelector('.MenuMobile');
+const menuMobile = document.querySelector('.menuMobile');
+const MenuIcon = document.querySelector('.MenuMobile');
+
+ButtonMobile.addEventListener('click', () => {
+    if (menuMobile.style.display == 'none') {
+        MenuIcon.setAttribute('name', 'close-outline');
+        menuMobile.classList.remove('animationClose');
+        menuMobile.classList.add('animationOpen');
+        menuMobile.style.display = 'block';
+    } else {
+        MenuIcon.setAttribute('name', 'menu-outline');
+        menuMobile.classList.remove('animationOpen');
+        menuMobile.classList.add('animationClose');
+        setTimeout(() => {
+            menuMobile.style.display = 'none';
+        }, 200)
+    }
+});
+
+const linksA = document.querySelectorAll('.link');
+
+linksA.forEach(linksA => {
+    linksA.addEventListener('click', () => {
+        MenuIcon.setAttribute('name', 'menu-outline');
+        menuMobile.classList.remove('animationOpen');
+        menuMobile.classList.add('animationClose');
+        setTimeout(() => {
+            menuMobile.style.display = 'none';
+        }, 300);
+    })
+})
+
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach(img => {
+    img.draggable = false;
+})
